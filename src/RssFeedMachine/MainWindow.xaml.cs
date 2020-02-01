87bd4b6f45b2dda46acd14c32a622f056de800e6
Boenne <nicolaj-simonsen@hotmail.com>
2020-02-01 13:47:34 +0100

    Init

---
+++ src/RssFeedMachine/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace RssFeedMachine
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
