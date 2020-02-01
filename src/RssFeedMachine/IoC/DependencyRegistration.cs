1a9a0f780d20bf37a1efa1bb9a682cc5e65694a3
Boenne <nicolaj-simonsen@hotmail.com>
2020-02-01 15:09:20 +0100

    Dependency registration is now working

---
+++ src/RssFeedMachine/IoC/DependencyRegistration.cs
@@ -0,0 +1,17 @@
+﻿using LightInject;
+using RssFeedMachine.ViewModels;
+
+namespace RssFeedMachine.IoC
+{
+    public class DependencyRegistration
+    {
+        public static ServiceContainer Container { get; set; }
+
+        public static void Register()
+        {
+            var serviceContainer = new ServiceContainer();
+            serviceContainer.Register<IMainViewModel, MainViewModel>();
+            Container = serviceContainer;
+        }
+    }
+}
