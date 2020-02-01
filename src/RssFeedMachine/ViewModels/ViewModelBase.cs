5e9b62e2a1399c62be5a3dcb3982a44a18f6814e
Boenne <nicolaj-simonsen@hotmail.com>
2020-02-01 15:09:52 +0100

    A base class for all viewmodels

---
+++ src/RssFeedMachine/ViewModels/ViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RssFeedMachine.ViewModels
+{
+    public class ViewModelBase : GalaSoft.MvvmLight.ViewModelBase
+    {
+    }
+}
