60f4cbccb2e70e39fcf84687498bc06edefe98ab
Boenne <nicolaj-simonsen@hotmail.com>
2020-02-01 20:02:58 +0100

    Moved ViewModelLocator out of ViewModels folder

---
+++ src/RssFeedMachine/ViewModelLocator.cs
@@ -0,0 +1,13 @@
+﻿using LightInject;
+using RssFeedMachine.IoC;
+using RssFeedMachine.ViewModels.Interfaces;
+
+namespace RssFeedMachine
+{
+    public class ViewModelLocator
+    {
+        public IMainViewModel MainViewModel => DependencyRegistration.Container.GetInstance<IMainViewModel>();
+        public IFeedListViewModel FeedListViewModel => DependencyRegistration.Container.GetInstance<IFeedListViewModel>();
+        public IAddFeedViewModel AddFeedViewModel => DependencyRegistration.Container.GetInstance<IAddFeedViewModel>();
+    }
+}
