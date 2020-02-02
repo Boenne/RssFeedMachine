31f445ceeba76b6be9ec7e14cc419741ae4d7e25
Boenne <nicolaj-simonsen@hotmail.com>
2020-02-02 17:40:34 +0100

    ViewModel for a feed

---
+++ src/RssFeedMachine/ViewModels/FeedViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace RssFeedMachine.ViewModels
+{
+    public class FeedViewModel : ViewModelBase
+    {
+        private string _title;
+        private string _url;
+
+        public string Title
+        {
+            get => _title;
+            set => Set(() => Title, ref _title, value);
+        }
+
+        public string Url
+        {
+            get => _url;
+            set => Set(() => Url, ref _url, value);
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
