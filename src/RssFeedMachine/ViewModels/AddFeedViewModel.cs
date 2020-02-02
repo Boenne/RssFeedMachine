3323dbf8799cbeeeebb57faa39387b0f99ea78db
Boenne <nicolaj-simonsen@hotmail.com>
2020-02-02 17:41:27 +0100

    AddFeed method now reads feed and adds it to repository

--- src/RssFeedMachine/ViewModels/AddFeedViewModel.cs
+++ src/RssFeedMachine/ViewModels/AddFeedViewModel.cs
@@ -1,14 +1,28 @@
 ﻿using System.Threading.Tasks;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
+using RssFeedMachine.Data.Repositories.Interfaces;
+using RssFeedMachine.Services.Exceptions;
+using RssFeedMachine.Services.Interfaces;
 using RssFeedMachine.ViewModels.Interfaces;
 using RssFeedMachine.Wrappers.Interfaces;
+using RssFeedMachine.Wrappers.Messenger.Messages;
 
 namespace RssFeedMachine.ViewModels
 {
     public class AddFeedViewModel : ViewModelBase, IAddFeedViewModel
     {
+        private readonly IFeedReaderService _feedReaderService;
+        private readonly IFeedRepository _feedRepository;
+        private readonly IMessenger _messenger;
         private string _feedUrl;
+
+        public AddFeedViewModel(IFeedReaderService feedReaderService, IFeedRepository feedRepository, IMessenger messenger)
+        {
+            _feedReaderService = feedReaderService;
+            _feedRepository = feedRepository;
+            _messenger = messenger;
+        }
 
         public string FeedUrl
         {
@@ -20,8 +34,18 @@ namespace RssFeedMachine.ViewModels
 
         public async Task AddFeed(IClosable window)
         {
+            try
+            {
+                var feed = await _feedReaderService.Read(FeedUrl);
+                await _feedRepository.Add(feed);
+                _messenger.Send(new FeedAddedMessage());
+            }
+            catch (UnableToReadFeedException exception)
+            {
+                //Handle
+            }
+            
             window.Close();
-            //TODO add feed to repo
         }
     }
 }
