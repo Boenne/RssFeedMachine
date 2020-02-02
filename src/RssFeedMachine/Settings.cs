3c744ea4d9a875f92905eeb394a104305b452dfb
Boenne <nicolaj-simonsen@hotmail.com>
2020-02-02 17:40:11 +0100

    Static file for storing data file path

---
+++ src/RssFeedMachine/Settings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace RssFeedMachine
+{
+    public static class Settings
+    {
+        public static string DataFilePath => $"{Environment.CurrentDirectory}/data.json";
+    }
+}
